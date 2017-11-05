bdef20d331a34dd30894bbc4b5146471a1d0e55c
Hai Victor Habi <haihabi1986@gmail.com>
2017-11-05 20:59:32 +0200

    adding group test support

---
+++ simpy/tests/group_test.py
@@ -0,0 +1,21 @@
+import unittest
+from simpy.tests.tutils import get_current_folder_path
+import os
+import simpy
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_group_read(self):
+        xml_path = os.path.join(get_current_folder_path(),'resources', 'simulation_cfg_groups.xml')
+        tr = simpy.read_from_xml(xml_path)
+        self.assertTrue(tr.test_list.__len__() == 4)
+
+
+    def test_group_read_not(self):
+        xml_path = os.path.join(get_current_folder_path(),'resources', 'simulation_cfg_groups_not.xml')
+        tr = simpy.read_from_xml(xml_path)
+        self.assertTrue(tr.test_list.__len__() == 2)
+
+if __name__ == '__main__':
+    unittest.main()
